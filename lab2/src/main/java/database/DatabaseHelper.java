c02498111293d0fd661308b9ac8e9c75ad60983b
Andrey Vasilyev <dayl1ex@gmail.com>
2017-12-01 01:42:41 +0300

    lab3 fix parameterized sql queries

--- lab2/src/main/java/database/DatabaseHelper.java
+++ lab2/src/main/java/database/DatabaseHelper.java
@@ -4,11 +4,7 @@ import model.Book;
 import util.ApplicationUtils;
 import util.FileEventLogger;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.LinkedList;
@@ -21,42 +17,42 @@ public class DatabaseHelper {
     public static final String password = "qwerty";
 
     private static Connection con;
-    private static Statement stmt;
-    private static ResultSet rs;
 
     public DatabaseHelper() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            FileEventLogger.logEvent(e.getMessage());
-        }
     }
 
     public static void addBook(Book book) {
-        FileEventLogger.logEvent("Adding new book: " + book.toString());
-        if (exists(book)) {
-            FileEventLogger.logEvent("Book " + book.toString() + " already exists.");
-            return;
-        }
 
+        FileEventLogger.logEvent("Adding new book: " + book.toString());
         try {
-            queryInsert("INSERT INTO Books (name, author, date, genre, rating) VALUES ('"
-                    + book.getName() + "','"
-                    + book.getAuthor() + "',"
-                    + "STR_TO_DATE('" + ApplicationUtils.formatDate(book.getDate()) + "','%d.%m.%Y'),'"
-                    + book.getGenre() + "'," +
-                    + book.getRating() + ");");
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = con.prepareStatement(
+                    "INSERT INTO Books (name, author, date, genre, rating) VALUES (?, ?, ?, ?, ?)");
+            stmt.setString(1,book.getName());
+            stmt.setString(2, book.getAuthor());
+            stmt.setDate(3,new java.sql.Date(book.getDate().getTime()));
+            stmt.setString(4,book.getGenre());
+            stmt.setInt(5,book.getRating());
+            stmt.addBatch();
+            stmt.executeBatch();
         } catch (SQLException e) {
             e.printStackTrace();
             FileEventLogger.logEvent(e.getMessage());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            FileEventLogger.logEvent(e.getMessage());
         }
     }
 
     public static List<Book> readBooks() throws ParseException {
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                FileEventLogger.logEvent(e.getMessage());
+            }
+        }
         FileEventLogger.logEvent("Reading books from database.");
         ResultSet rs = null;
         try {
@@ -91,40 +87,6 @@ public class DatabaseHelper {
     private static ResultSet queryRead(String query) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         con = DriverManager.getConnection(url, user, password);
-        stmt = con.createStatement();
-        rs = stmt.executeQuery(query);
-        return rs;
-    }
-
-    private static void queryInsert(String query) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection(url, user, password);
-        stmt = con.createStatement();
-        stmt.executeUpdate(query);
-
-        if (con != null) {
-            con.close();
-        }
-        if (con != null) {
-            stmt.close();
-        }
-        if (rs != null) {
-            rs.close();
-        }
-    }
-
-    private static boolean exists(Book book) {
-        ResultSet rs = null;
-        try {
-            rs = queryRead("SELECT * FROM Books WHERE name='" + book.getName() + "' and author='" + book.getAuthor() + "';");
-            return rs.next();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            FileEventLogger.logEvent(e.getMessage());
-        } catch (SQLException e) {
-            e.printStackTrace();
-            FileEventLogger.logEvent(e.getMessage());
-        }
-        return false;
+        return con.createStatement().executeQuery(query);
     }
 }
